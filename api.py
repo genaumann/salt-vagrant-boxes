12e2bc5697893c2773e83d5c3b6fce10038a9ab0
Gino Naumann <g@gnaumann.de>
2024-10-09 23:41:32 +0200

    Migrate api.py to hcp cloud

--- api.py
+++ api.py
@@ -6,21 +6,41 @@ import os
 import sys
 import requests
 
-base_url = "https://app.vagrantup.com/api/v2"
+base_url = "https://api.cloud.hashicorp.com/vagrant/2022-09-30/registry"
 username = "genaumann"
 
 session = requests.Session()
-try:
-  session.headers.update({"Authorization": f"Bearer {os.environ['VAGRANT_CLOUD_TOKEN']}"})
-except KeyError:
-  raise Exception("Set vagrant cloud token variable: $VAGRANT_CLOUD_TOKEN")
+
+def login() -> bool:
+  """
+  Login to hcp
+  """
+
+  client_id = os.environ.get("HCP_CLIENT_ID")
+  client_secret = os.environ.get("HCP_CLIENT_SECRET")
+
+  url = "https://auth.idp.hashicorp.com/oauth2/token"
+  headers = {
+      "Content-Type": "application/x-www-form-urlencoded"
+  }
+  data = {
+      "client_id": client_id,
+      "client_secret": client_secret,
+      "grant_type": "client_credentials",
+      "audience": "https://api.hashicorp.cloud"
+  }
+
+  response = requests.post(url, headers=headers, data=data)
+  response.raise_for_status()
+
+  session.headers.update({"Authorization": f"Bearer {response.json()['access_token']}"})
 
 def get_boxes() -> dict[dict]:
   """
   Get all boxes of user
   """
 
-  response = session.get(f"{base_url}/user/{username}")
+  response = session.get(f"{base_url}/{username}/boxes")
   response.raise_for_status()
   return response.json()["boxes"]
 
@@ -43,19 +63,16 @@ def create_box(box_name : str) -> bool:
   """
 
   data = {
-    "box": {
-      "username": username,
-      "name": f"{box_name}-salt",
-      "short_description": f"{box_name} Salt Formula testing",
-      "is_private": False
-    }
+    "name": f"{box_name}-salt",
+    "short_description": f"{box_name} Salt Formula testing",
+    "is_private": False
   }
 
-  response = session.post(f"{base_url}/boxes", json=data)
+  response = session.post(f"{base_url}/{username}/boxes", json=data)
   response.raise_for_status()
 
   try:
-    print(f"Box {response.json()["tag"]} successfully created")
+    print(f"Box {response.json()["box"]["name"]} successfully created")
     return True
   except KeyError:
     raise Exception(f"Box {box_name} not created")
@@ -65,7 +82,7 @@ def get_versions(box_name : str) -> list:
   Get all versions of a box
   """
 
-  response = session.get(f"{base_url}/box/{username}/{box_name}-salt")
+  response = session.get(f"{base_url}/{username}/box/{box_name}-salt/versions")
   response.raise_for_status()
 
   try:
@@ -80,7 +97,7 @@ def check_version_exist(box_name : str, salt_version : str) -> bool:
 
   versions = get_versions(box_name)
   for version in versions:
-    if version["version"] == salt_version:
+    if version["name"] == salt_version:
       return True
     
   return False
@@ -91,17 +108,16 @@ def rename_version(box_name : str, salt_version : str) -> bool:
   """
 
   data = {
-    "version": {
-      "version": f"{salt_version}.old",
-      "description": f"Deprecated - use {salt_version} instead"
-    }
+    "name": f"{salt_version}.old",
+    "description": f"Deprecated - use {salt_version} instead",
+    "state": "DEPRECATED"
   }
 
-  response = session.put(f"{base_url}/box/{username}/{box_name}-salt/version/{salt_version}", json=data)
+  response = session.put(f"{base_url}/{username}/box/{box_name}-salt/version/{salt_version}", json=data)
   response.raise_for_status()
 
   try:
-    print(f"{box_name} version changed from {salt_version} to {response.json()['version']}")
+    print(f"{box_name} version changed from {salt_version} to {response.json()["version"]["name"]}")
     return True
   except KeyError:
     raise Exception(f"Renaming of version {salt_version} from {box_name} failed")
@@ -111,18 +127,21 @@ def remove_version(box_name: str, salt_version: str) -> bool:
   Remove deprecated version
   """
 
-  response = session.delete(f"{base_url}/box/{username}/{box_name}-salt/version/{salt_version}")
+  response = session.delete(f"{base_url}/{username}/box/{box_name}-salt/version/{salt_version}")
   response.raise_for_status()
 
-  try:
-    print(f"Removed version {response.json()["version"]} from {box_name}")
-    return True
-  except KeyError:
-    raise Exception(f"Failed to remove version {salt_version} of {box_name}")
+  print(f"Removed version {salt_version} from {box_name}")
 
 if __name__ == "__main__":
   box_name = sys.argv[1]
   salt_version = sys.argv[2]
+
+  try:
+    login()
+  except Exception as e:
+    print(f"Failed to login: {e}")
+    sys.exit(1)
+    
   if not check_box_exist(box_name):
     create_box(box_name)
     sys.exit(0)
